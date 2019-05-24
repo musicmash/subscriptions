e0bd2fba49072b27a419c9f77454c5547b31fcc3
Mikhail Kalinin <m.kalinin@selectel.ru>
2019-05-24 20:41:42 +0300

    Implement main.go

--- cmd/artists/main.go
+++ cmd/artists/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"flag"
 
+	"github.com/musicmash/artists/internal/config"
+	"github.com/musicmash/artists/internal/db"
+	"github.com/musicmash/artists/internal/log"
+)
+
+func main() {
+	configPath := flag.String("config", "/etc/musicmash-artists/artists.yaml", "Path to artists.yaml config")
+	flag.Parse()
+
+	if err := config.InitConfig(*configPath); err != nil {
+		panic(err)
+	}
+	if config.Config.Log.Level == "" {
+		config.Config.Log.Level = "info"
+	}
+
+	log.SetLogFormatter(&log.DefaultFormatter)
+	log.ConfigureStdLogger(config.Config.Log.Level)
+
+	db.DbMgr = db.NewMainDatabaseMgr()
+
+	log.Info("Running artists service..")
 }
