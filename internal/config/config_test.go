e3be60f1965fc978367c68bd0eee5eb71b7daf45
Mikhail Kalinin <m.kalinin@selectel.ru>
2019-05-30 14:54:05 +0300

    Add config test

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,7 +1,53 @@
 package config
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestConfig_Load(t *testing.T) {
-	t.SkipNow()
+	// arrange
+	data := `
+---
+db:
+  type:  'mysql'
+  host:  'mariadb'
+  name:  'musicmash-artists'
+  login: 'musicmash-artists'
+  pass:  'musicmash-artists'
+  log: false
+
+log:
+  level: DEBUG
+  file: 'musicmash-artists.log'
+  syslog_enable: false
+
+http:
+  port: 5566
+`
+	expected := &AppConfig{
+		DB: DBConfig{
+			Type:  "mysql",
+			Host:  "mariadb",
+			Name:  "musicmash-artists",
+			Login: "musicmash-artists",
+			Pass:  "musicmash-artists",
+			Log:   false,
+		},
+		Log: LogConfig{
+			Level:         "DEBUG",
+			File:          "musicmash-artists.log",
+		},
+		HTTP: HTTPConfig{
+			Port: 5566,
+		},
+	}
+
+	// action
+	err := Load([]byte(data))
+
+	// assert
+	assert.NoError(t, err)
+	assert.Equal(t, expected, Config)
 }
